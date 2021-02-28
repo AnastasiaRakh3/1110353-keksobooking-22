cf0fa9d75adcfc81b0949b1e85e82168c3e14353
Anastasiia <rakhmatova-93@mail.ru>
2021-02-28 14:28:11 +0300

    Внесла изменения

--- js/template.js
+++ js/template.js
@@ -1,21 +1,52 @@
-import {createAdvertising} from './data.js';
+import { createAdvertising } from './data.js';
 
 const firstAd = createAdvertising();
+const cardTemplate = document.querySelector('#card').content;
+const newCardTemplate = cardTemplate.querySelector('.popup');
+
+const RoomTypes = {
+  FLAT: 'Квартира',
+  BUNGALOW: 'Бунгало',
+  HOUSE: 'Дом',
+  PALACE: 'Дворец',
+};
 
 const translateType = function (ad) {
-  if (ad.offer.type === 'flat') {
-    return 'Квартира';
-  } else if (ad.offer.type === 'bungalow') {
-    return 'Бунгало';
-  } else if (ad.offer.type === 'house') {
-    return 'Дом';
-  }
-  return 'Дворец';
+  return RoomTypes[ad.offer.type.toUpperCase()];
+};
+
+const featuresFragment = document.createDocumentFragment();
+
+const getFeatures = function (array) {
+  array.forEach((value) => {
+    const newFeatureElement = document.createElement('li');
+    newFeatureElement.classList.add('popup__feature');
+    let newFeatureClass = 'popup__feature--';
+    newFeatureClass += value;
+    newFeatureElement.classList.add(newFeatureClass);
+
+    featuresFragment.appendChild(newFeatureElement);
+  });
+  return featuresFragment;
+};
+
+const photosFragment = document.createDocumentFragment();
+
+const getPhotos = function (array) {
+  array.forEach((value) => {
+    const newPhotoElement = document.createElement('img');
+    newPhotoElement.classList.add('popup__photo');
+    newPhotoElement.width = '45';
+    newPhotoElement.height = '40';
+    newPhotoElement.alt = 'Фотография жилья';
+    newPhotoElement.src = value;
+
+    photosFragment.appendChild(newPhotoElement);
+  });
+  return photosFragment;
 };
 
 const createCard = function (ad) {
-  const cardTemplate = document.querySelector('#card').content;
-  const newCardTemplate = cardTemplate.querySelector('.popup');
   const cardOffer = newCardTemplate.cloneNode(true);
 
   const titleCardOffer = cardOffer.querySelector('.popup__title');
@@ -24,27 +55,30 @@ const createCard = function (ad) {
   const typeCardOffer = cardOffer.querySelector('.popup__type');
   const capacityCardOffer = cardOffer.querySelector('.popup__text--capacity');
   const timeCardOffer = cardOffer.querySelector('.popup__text--time');
-  const featuresCardOffer = cardOffer.querySelector('.popup__features');
   const descriptionCardOffer = cardOffer.querySelector('.popup__description');
-  const photosCardOffer = cardOffer.querySelector('.popup__photos');
   const avatarCardOffer = cardOffer.querySelector('.popup__avatar');
 
   titleCardOffer.textContent = ad.offer.title;
   addressCardOffer.textContent = ad.offer.address;
-  priceCardOffer.textContent = '${ad.offer.price} <span>₽/ночь</span>';
+  priceCardOffer.textContent = `${ad.offer.price} `;
+  priceCardOffer.innerHTML += '<span>₽/ночь</span>';
   typeCardOffer.textContent = translateType(ad);
-  capacityCardOffer.textContent = '${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей';
-  timeCardOffer.textContent = 'Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}';
-  featuresCardOffer.textContent = ad.offer.address;
+  capacityCardOffer.textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  timeCardOffer.textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
   descriptionCardOffer.textContent = ad.offer.description;
-  photosCardOffer.textContent = ad.offer.photos;
-  avatarCardOffer.src = ad.offer.author;
+  avatarCardOffer.src = ad.author.avatar;
+  
+  const featuresListCardOffer = document.querySelector('.popup__features');
+  featuresListCardOffer.innerHTML = '';
+  const photosCardOffer = cardOffer.querySelector('.popup__photos');
+  photosCardOffer.innerHTML = '';
+  featuresListCardOffer.appendChild(getFeatures(ad.offer.features));
+  photosCardOffer.appendChild(getPhotos(ad.offer.photos));
+
   return cardOffer;
 };
 
 const firstAdCard = createCard(firstAd);
 
-const mapCanvas = document.querySelector('#map__canvas');
+const mapCanvas = document.querySelector('.map__canvas');
 mapCanvas.appendChild(firstAdCard);
-
-
