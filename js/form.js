37969a9e020f9cc40e7f9d83d95f90110f441f5b
Anastasiia <rakhmatova-93@mail.ru>
2021-03-13 23:22:18 +0300

    Написала вадилацию для полей формы

--- js/form.js
+++ js/form.js
@@ -3,13 +3,17 @@ import {addForm} from './map.js';
 import {sendData} from './api.js';
 import {showErrorBlock} from './template.js';
 
-const housingTypeSelect = document.querySelector('#type');
-const pricePerDayInput = document.querySelector('#price');
-const timeInInput = document.querySelector('#timein');
-const timeOutInput = document.querySelector('#timeout');
+const housingTypeSelect = addForm.querySelector('#type');
+const pricePerDayInput = addForm.querySelector('#price');
+const timeInInput = addForm.querySelector('#timein');
+const timeOutInput = addForm.querySelector('#timeout');
 
-const roomNumberinput = document.querySelector('#room_number');
-const capacityinput = document.querySelector('#capacity');
+const roomNumberinput = addForm.querySelector('#room_number');
+const capacityinput = addForm.querySelector('#capacity');
+const formBtnSubmit = addForm.querySelector('.ad-form__submit');
+
+const addressInput = addForm.querySelector('#address');
+addressInput.value = '35.685471,139.753590'
 
 // Валидация полей чекина и чекаута
 
@@ -29,7 +33,6 @@ const selectMinPrice = function() {
 selectMinPrice();
 housingTypeSelect.addEventListener('change', selectMinPrice);
 
-
 const synchronizeTimeIn = function() {
   timeInInput.value = timeOutInput.value;
 }
@@ -43,53 +46,38 @@ timeOutInput.addEventListener('change', synchronizeTimeIn);
 
 // Валидация полей кол-ва комнат и кол-ва гостей
 
-const checkRooms = function () {
-  if(roomNumberinput.value === '1' && capacityinput.value === '1') {
-    capacityinput.style.backgroundColor = 'red';
-    capacityinput.setCustomValidity('');
-  }
-  if(roomNumberinput.value === '2' && (capacityinput.value === '1'|| capacityinput.value === '2')) {
-    capacityinput.style.backgroundColor = 'green';
-    capacityinput.setCustomValidity('');
-  }
-  if(roomNumberinput.value === '3' && (capacityinput.value === '1'|| capacityinput.value === '2' || capacityinput.value === '3')) {
-    capacityinput.style.backgroundColor = 'blue';
-    capacityinput.setCustomValidity('');
-  }
-  if(roomNumberinput.value === '100' && (capacityinput.value === '0')) {
-    capacityinput.style.backgroundColor = 'black';
-    capacityinput.setCustomValidity('');
+const checkRoomsGuests = function () {
+  if(Number(capacityinput.value) > Number(roomNumberinput.value) || capacityinput.value === '0' && !(roomNumberinput.value === '100')) {
+    capacityinput.setCustomValidity('Не допустимый вариант выбора количества гостей для выбранного колличества комнат');
+  } else if(!(capacityinput.value === '0') && roomNumberinput.value === '100') {
+    capacityinput.setCustomValidity('Единственный возможный вариант для 100 комнат - это "не для гостей"');
   }
   else {
-    capacityinput.setCustomValidity('Не допустимый вариант выбора количества гостей для выбранного колличества комнат');
+    capacityinput.setCustomValidity('');
   }
-  capacityinput.reportValidity();
 }
-// const checkRooms = function () {
-//   if(roomNumberinput.value === '1' && !(capacityinput.value === '1')) {
-//     capacityinput.style.backgroundColor = 'red';
-//     capacityinput.setCustomValidity('Не допустимый вариант выбора количества гостей для выбранного колличества комнат');
-//   }
-//   if(roomNumberinput.value === '2' && !(capacityinput.value === '1'|| capacityinput.value === '2')) {
-//     capacityinput.style.backgroundColor = 'green';
-//     capacityinput.setCustomValidity('Не допустимый вариант выбора количества гостей для выбранного колличества комнат');
-//   }
-//   if(roomNumberinput.value === '3' && !(capacityinput.value === '1'|| capacityinput.value === '2' || capacityinput.value === '3')) {
-//     capacityinput.style.backgroundColor = 'blue';
-//     capacityinput.setCustomValidity('Не допустимый вариант выбора количества гостей для выбранного колличества комнат');
-//   }
-//   if(roomNumberinput.value === '100' && !(capacityinput.value === '0')) {
-//     capacityinput.style.backgroundColor = 'black';
-//     capacityinput.setCustomValidity('Не допустимый вариант выбора количества гостей для выбранного колличества комнат');
-//   }
-//   else {
-//     capacityinput.setCustomValidity('');
-//   }
-//   capacityinput.reportValidity();
-// };
 
-capacityinput.addEventListener('change', checkRooms);
-roomNumberinput.addEventListener('change', checkRooms);
+formBtnSubmit.addEventListener('click', checkRoomsGuests);
+
+// Проверка на валидацию всей формы, выделяя красной рамкой неправильно введенные поля
+
+
+const checkValidity = function () {
+  const invalidElements = addForm.querySelectorAll('input:invalid, select:invalid');
+  const allElements = addForm.querySelectorAll('input, select');
+
+  for (let i = 0; i < allElements.length; i++){
+    allElements[i].style.border = '1px solid #d9d9d3';
+  }
+
+  if (invalidElements.length) {
+    for (let i = 0; i < invalidElements.length; i++){
+      invalidElements[i].style.border = 'solid 3px red';
+    }
+  }
+}
+
+addForm.addEventListener('click', checkValidity);
 
 // Отправка форма
 
